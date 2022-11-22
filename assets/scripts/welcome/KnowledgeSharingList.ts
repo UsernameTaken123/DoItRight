3ee897feb5c25b5faf0a71068f2f02f81a7eefdf
UsernameTaken <107462782+UsernameTaken123@users.noreply.github.com>
2022-11-22 18:53:16 +1300

    Update KnowledgeSharingList.ts

--- assets/scripts/welcome/KnowledgeSharingList.ts
+++ assets/scripts/welcome/KnowledgeSharingList.ts
@@ -21,7 +21,52 @@ const listInfo = [
       {
         src: "google.com",
         src_short: "google.com",
-        content: "Everybody can do a better job to reduce the waste.",
+        content: "Use a reusable bottle/cup for beverages on-the-go.",
+      },
+      {
+        src: "google.com",
+        src_short: "google.com",
+        content: "Use reusable grocery bags, and not just for groceries.",
+      },
+      {
+        src: "google.com",
+        src_short: "google.com",
+        content: "Purchase wisely and recycle - think about its impact on the planet.",
+      },
+      {
+        src: "google.com",
+        src_short: "google.com",
+        content: "Compost it!",
+      },
+      {
+        src: "google.com",
+        src_short: "google.com",
+        content: "Avoid single-use food and drink containers and utensils. ",
+      },
+      {
+        src: "google.com",
+        src_short: "google.com",
+        content: "Buy second-hand items and donate used goods, they give the item a second use before throwing it away.",
+      },
+      {
+        src: "google.com",
+        src_short: "google.com",
+        content: "Shop local farmers markets and buy in bulk to reduce packaging.",
+      },
+      {
+        src: "google.com",
+        src_short: "google.com",
+        content: "Curb your use of paper: mail, receipts, magazines",
+      },
+      {
+        src: "google.com",
+        src_short: "google.com",
+        content: "Buy recycled bags, They are made of recycled materials which gives them more usage life and most of them can be recycled/disposed in a good and friendly way to the planet.",
+      },
+      {
+        src: "google.com",
+        src_short: "google.com",
+        content: "Borrow, Rent, Share! Borrow your friend's tools, items, etc.. instead of buying a new one, these reduce the total waste produced by limiting the amounts of products existing.",
       },
   ];
 
